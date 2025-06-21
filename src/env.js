a84efa340e8d17905f794844d2ad084ba6d344fb
yatochka-dev <philipchef13@gmail.com>
2025-06-21 17:05:50 +0300

    fix: change db .env var

--- src/env.js
+++ src/env.js
@@ -38,7 +38,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DB_POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
     PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
     SUDO_USERS: process.env.SUDO_USERS,
